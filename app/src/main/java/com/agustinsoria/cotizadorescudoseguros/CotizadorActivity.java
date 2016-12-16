347d36794f9133f20df1424f0be859f9337fee31
JoelMut <joelmut_2007@hotmail.com>
2016-12-16 18:38:34 -0300

    s

--- app/src/main/java/com/agustinsoria/cotizadorescudoseguros/CotizadorActivity.java
+++ app/src/main/java/com/agustinsoria/cotizadorescudoseguros/CotizadorActivity.java
@@ -1,13 +1,25 @@
 package com.agustinsoria.cotizadorescudoseguros;
 
-import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by Joel on 16/12/2016.
+ */
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class CotizadorActivity extends AppCompatActivity {
 
-    @Override
+    private TextView text;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cotizador);
+        text = (TextView)findViewById(R.id.textView);
+    }
+
+    public void load(View view){
+        String temp = "Got it!";
+        text.setText(temp);
     }
 }
